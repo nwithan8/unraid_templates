16109190f315d3b92bc7641e059ee65684c020ca
Nate Harris <nwithan8@users.noreply.github.com>
2025-05-02 10:43:29 -0600

    Update scripts:

--- scripts/replace_all_placeholder_icons.sh
+++ scripts/replace_all_placeholder_icons.sh
@@ -6,8 +6,13 @@ PLACEHOLDER_IMAGE="$IMAGE_FOLDER/placeholder-icon.png"
 # DO NOT EDIT BELOW
 
 # This is the MacOS equivalent command, will fail on Linux)
-PLACEHOLDER_IMAGE_SIZE_BYTES=$(stat -c "%z" "$PLACEHOLDER_IMAGE")
-FIND_RESULTS=$(find ../images/ -type f -size $PLACEHOLDER_IMAGE_SIZE_BYTES)
+PLACEHOLDER_IMAGE_SIZE_BYTES=$(stat -f "%z" "$PLACEHOLDER_IMAGE")
+
+echo "$PLACEHOLDER_IMAGE_SIZE_BYTES"
+
+FIND_RESULTS=$(find $IMAGE_FOLDER -type f -size ${PLACEHOLDER_IMAGE_SIZE_BYTES}c)
+
+echo "$FIND_RESULTS"
 
 for file in $FIND_RESULTS; do
     cp "$PLACEHOLDER_IMAGE" "${file}"
